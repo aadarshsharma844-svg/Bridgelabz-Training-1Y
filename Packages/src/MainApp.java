b783b5b66496875d8f23a8df92942a967641620c
aadarshsharma844-svg <aadarshsharma844@gmail.com>
2026-03-15 17:45:34 +0530

    "[Aadarsh] file added"

---
+++ Packages/src/MainApp.java
@@ -0,0 +1,11 @@
+import com.university.department.cse.Course;
+
+
+
+public class MainApp {
+    public static void main(String[] args) {
+        Course myCourse = new Course();
+        myCourse.printCourseDetails("DSA", "BCSC1511");
+    }
+}
+
---
+++ Packages/src/com/university/department/cse/Course.java
@@ -0,0 +1,8 @@
+package com.university.department.cse;
+
+public class Course {
+    public void printCourseDetails(String courseName, String courseCode) {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Course Code: " + courseCode);
+    }
+}
