10ad3cae2ddf5e5dc1af0699659b75663a2a91f2
aadarshsharma844-svg <aadarshsharma844@gmail.com>
2026-03-24 21:18:35 +0530

    "[Aadarsh] File added"

---
+++ Inheritence/src/Problem_3/MotorBike.java
@@ -0,0 +1,10 @@
+package Problem_3;
+public class MotorBike extends Vehicle{
+    int Seatcapacity_for_bike;
+    public void Display_info(){
+        System.out.println("Seat capacity for bike is-"+Seatcapacity_for_bike);
+        System.out.println("Type of fuel for bike is-"+Fueltype);
+        System.out.println("Max speed of bike is-"+maxspeed);
+        System.out.println("                     ");
+    }
+}
---
+++ Inheritence/src/Problem_3/Vehicle.java
@@ -0,0 +1,10 @@
+package Problem_3;
+public class Vehicle {
+        int maxspeed;
+        String Fueltype;
+        public void Display_info(){
+            System.out.println("Details of vechile is");
+        }
+ }
+
+
