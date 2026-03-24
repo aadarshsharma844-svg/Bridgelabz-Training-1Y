d4464b1e9025e804c40d34291b5992de111921e1
aadarshsharma844-svg <aadarshsharma844@gmail.com>
2026-03-24 21:07:28 +0530

    "[Aadarsh] File added"

---
+++ Inheritence/src/Problem_2/Employee.java
@@ -0,0 +1,9 @@
+package Problem_2;
+public class Employee {
+    String name;
+    int id;
+    int salary;
+    public void Display_details(){
+        System.out.println("Details of emp are");
+    }
+}
