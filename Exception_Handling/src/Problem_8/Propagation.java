628c0103f99d103b078543fb7cb5812cc5e1062b
aadarshsharma844-svg <aadarshsharma844@gmail.com>
2026-03-28 21:09:09 +0530

    "[Aadarsh] File added"

---
+++ Exception_Handling/src/Problem_8/Propagation.java
@@ -0,0 +1,17 @@
+package Problem_8;
+public class Propagation {
+
+    static void method1() {
+        int result = 10 / 0;
+    }
+    static void method2() {
+        method1();
+    }
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Exception Handled");
+        }
+    }
+}
