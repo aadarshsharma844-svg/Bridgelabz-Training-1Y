f15bedc27c301e9bc86c94d1a7050fd789341917
aadarshsharma844-svg <aadarshsharma844@gmail.com>
2026-03-28 20:50:20 +0530

    "[Aadarsh] File added"

---
+++ Encapsulation/src/Problem_5/Reservable.java
@@ -0,0 +1,6 @@
+package Problem_5;
+interface Reservable
+{
+    void reserveItem();
+    boolean checkAvailability();
+}
---
+++ Encapsulation/src/Problem_5/Runner.java
@@ -0,0 +1,21 @@
+package Problem_5;
+public class Runner
+{
+    public static void main(String[] args) {
+        Library item1 = new Book(1, "Java", "Franklin");
+        Library item2 = new Magazine(2, "Tech Today", "Michael");
+        Library item3 = new DVD(3, "Movie", "Trevor");
+
+        item1.getItemDetails();
+        System.out.println("Loan Days: " + item1.getLoanDuration());
+
+        item2.getItemDetails();
+        System.out.println("Loan Days: " + item2.getLoanDuration());
+
+        item3.getItemDetails();
+        System.out.println("Loan Days: " + item3.getLoanDuration());
+
+        ((Reservable)item1).reserveItem();
+        ((Reservable)item2).reserveItem();
+    }
+}
