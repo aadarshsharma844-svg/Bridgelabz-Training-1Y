ea23f0218a8c613bf5424981683c93dd823ed128
aadarshsharma844-svg <aadarshsharma844@gmail.com>
2026-03-28 20:56:48 +0530

    "[Aadarsh] File added"

---
+++ Encapsulation/src/Problem_7/Runner.java
@@ -0,0 +1,22 @@
+package Problem_7;
+public class Runner
+{
+    public static void main(String[] args)
+    {
+
+        Patient p1 = new InPatient(1, "Krishna", 18);
+        Patient p2 = new OutPatient(2, "Shiv", 21);
+
+        p1.getPatientDetails();
+        System.out.println("Bill: " + p1.calculateBill());
+
+        p2.getPatientDetails();
+        System.out.println("Bill: " + p2.calculateBill());
+
+        ((MedicalRecord)p1).addRecord("Surgery Done");
+        ((MedicalRecord)p2).addRecord("General Checkup");
+
+        ((MedicalRecord)p1).viewRecords();
+        ((MedicalRecord)p2).viewRecords();
+    }
+}
